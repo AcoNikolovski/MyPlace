7e6794a016eedb828e3251e2dd534289ddad354d
AcoNikolovski <aconikolovski85@yahoo.com>
2020-07-31 20:32:47 +0200

    Test

---
+++ MyPlace/Models/Model1.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TestMVC.Models
+{
+    public class Model1
+    {
+        public int Id { get; set; }
+        public string UserEmail { get; set; }
+        public DateTime Created { get; set; }
+        public string ImageURL { get; set; }
+        public List<string> Images { get; set; }
+
+    }
+}
